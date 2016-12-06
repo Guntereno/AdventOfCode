0a0393653fa6b9d3c528ce24bc1c913b50b91356
Pete Gunter <Pete.Gunter@gmail.com>
2016-12-06 12:17:26 +0000

    Added solution to Day Two

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace Advent
 		{
 			Console.WriteLine(DayOne.GetSolution());
 			Console.WriteLine(DayTwo.GetSolution());
+
+			string day3Input = File.ReadAllText("data/day3.txt");
+			Console.WriteLine(DayThree.GetSolution(day3Input));
 		}
 	}
 }
